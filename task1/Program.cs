f5bc1611ae7e950b90d6797ff32be704ba1ceae8
Marina <gtellier.mecanics@gmail.com>
2022-07-04 14:58:57 +0300

    Палиндромы

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+Task1(number);
+void Task1(string currentNumber)
+{
+    if (currentNumber[0] == currentNumber[4] && currentNumber[1] == currentNumber[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
